aa14593268e902851528a0acd198c8870c2be820
ArnoldasA <arnoldasalencikas@gmail.com>
2022-07-15 13:44:37 +0100

    ErrorLogger Unit Test Added

---
+++ ErrorLogger.cs
@@ -0,0 +1,19 @@
+public class ErrorLogger
+    {
+        public string LastError { get; set; }
+
+        public event EventHandlerGuid ErrorLogged; 
+        
+        public void Log(string error)
+        {
+            if (String.IsNullOrWhiteSpace(error))
+                throw new ArgumentNullException();
+                
+            LastError = error; 
+            
+             Write the log to a storage
+             ...
+
+            ErrorLogged.Invoke(this, Guid.NewGuid());
+        }
+    }
